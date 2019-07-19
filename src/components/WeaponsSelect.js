ffc130998a71f517877937dedb605c5a4f6304d0
Brian Doane <brianmdoane@gmail.com>
2019-07-19 00:00:43 -0500

    File and style cleanup

--- src/components/WeaponsSelect.js
+++ src/components/WeaponsSelect.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import { FaHandRock, FaHandScissors, FaHandPaper, FaHandLizard, FaHandSpock } from 'react-icons/fa'
-import { Button, ButtonToolbar } from 'react-bootstrap'
+import { Row, Button, ButtonToolbar } from 'react-bootstrap'
 
 export default function WeaponsSelect({ handleSelect, winner, arsenal }) {
 
   return (
-    <ButtonToolbar aria-label="Toolbar with buttons">
-      <Button type="button" onClick={() => handleSelect(arsenal[0])} disabled={winner}variant="dark" className="mr-2">
-        <FaHandRock size={42} fill="#ff0000" />
-      </Button>
-      <Button type="button" onClick={() => handleSelect(arsenal[1])} disabled={winner} variant="dark" className="mr-2">
-        <FaHandPaper size={42} fill="#ff0000" />
-      </Button>
-      <Button type="button" onClick={() => handleSelect(arsenal[2])} disabled={winner} variant="dark" className="mr-2">
-        <FaHandScissors size={42} fill="#ff0000" />
-      </Button>
-      <Button type="button" onClick={() => handleSelect(arsenal[3])} disabled={winner} variant="dark" className="mr-2">
-        <FaHandLizard size={42} fill="#ff0000" />
-      </Button>
-      <Button type="button" onClick={() => handleSelect(arsenal[4])} disabled={winner} variant="dark">
-        <FaHandSpock size={42} fill="#ff0000" />
-      </Button>
-    </ButtonToolbar>
+    <Row className="flex-center">
+      <ButtonToolbar aria-label="Toolbar with buttons">
+        <Button type="button" onClick={() => handleSelect(arsenal[0])} disabled={winner}variant="dark" className="mr-2">
+          <FaHandRock size={42} fill="#ff0000" />
+        </Button>
+        <Button type="button" onClick={() => handleSelect(arsenal[1])} disabled={winner} variant="dark" className="mr-2">
+          <FaHandPaper size={42} fill="#ff0000" />
+        </Button>
+        <Button type="button" onClick={() => handleSelect(arsenal[2])} disabled={winner} variant="dark" className="mr-2">
+          <FaHandScissors size={42} fill="#ff0000" />
+        </Button>
+        <Button type="button" onClick={() => handleSelect(arsenal[3])} disabled={winner} variant="dark" className="mr-2">
+          <FaHandLizard size={42} fill="#ff0000" />
+        </Button>
+        <Button type="button" onClick={() => handleSelect(arsenal[4])} disabled={winner} variant="dark">
+          <FaHandSpock size={42} fill="#ff0000" />
+        </Button>
+      </ButtonToolbar>
+    </Row>
   )
 }
 
