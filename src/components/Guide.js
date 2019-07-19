4838b4bc07d447d38c742840c8f6eff618a59e62
Brian Doane <brianmdoane@gmail.com>
2019-07-18 20:51:32 -0500

    Spread arsenal variables through the app, replacing text, making weapon swap out easier

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -2,15 +2,18 @@ import React from 'react'
 import Row from 'react-bootstrap/Row'
 import Results from './Results'
 
-export default function Guide({ start, results, human, computer }) {
+export default function Guide({ start, results, human, computer, arsenal }) {
+  const { name } = computer
+
   return (
     <Row className="flex-center" style={{ paddingBottom: '17px' }}>
       { start ?
-        <h4>Choose your weapon below to engage Sheldon!</h4> :
+        <h4>Choose your weapon below to engage {name}!</h4> :
         <Results
           results={results}
           human={human}
           computer={computer}
+          arsenal={arsenal}
         />
       }
     </Row>
--- src/components/WeaponsSelect.js
+++ src/components/WeaponsSelect.js
@@ -2,23 +2,23 @@ import React from 'react'
 import { FaHandRock, FaHandScissors, FaHandPaper, FaHandLizard, FaHandSpock } from 'react-icons/fa'
 import { Button, ButtonToolbar } from 'react-bootstrap'
 
-export default function WeaponsSelect({ handleSelect, winner }) {
+export default function WeaponsSelect({ handleSelect, winner, arsenal }) {
 
   return (
     <ButtonToolbar aria-label="Toolbar with buttons">
-      <Button type="button" id="rock" onClick={() => handleSelect('rock')} disabled={winner}variant="dark" className="mr-2">
+      <Button type="button" onClick={() => handleSelect(arsenal[0])} disabled={winner}variant="dark" className="mr-2">
         <FaHandRock size={42} fill="#ff0000" />
       </Button>
-      <Button type="button" onClick={() => handleSelect('paper')} disabled={winner} variant="dark" className="mr-2">
+      <Button type="button" onClick={() => handleSelect(arsenal[1])} disabled={winner} variant="dark" className="mr-2">
         <FaHandPaper size={42} fill="#ff0000" />
       </Button>
-      <Button type="button" onClick={() => handleSelect('scissors')} disabled={winner} variant="dark" className="mr-2">
+      <Button type="button" onClick={() => handleSelect(arsenal[2])} disabled={winner} variant="dark" className="mr-2">
         <FaHandScissors size={42} fill="#ff0000" />
       </Button>
-      <Button type="button" onClick={() => handleSelect('lizard')} disabled={winner} variant="dark" className="mr-2">
+      <Button type="button" onClick={() => handleSelect(arsenal[3])} disabled={winner} variant="dark" className="mr-2">
         <FaHandLizard size={42} fill="#ff0000" />
       </Button>
-      <Button type="button" onClick={() => handleSelect('spock')} disabled={winner} variant="dark">
+      <Button type="button" onClick={() => handleSelect(arsenal[4])} disabled={winner} variant="dark">
         <FaHandSpock size={42} fill="#ff0000" />
       </Button>
     </ButtonToolbar>
