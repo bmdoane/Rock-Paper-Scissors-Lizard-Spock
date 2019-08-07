2c66acf186cf16673112c0edea27281b895aca72
Brian Doane <brianmdoane@gmail.com>
2019-08-07 11:02:19 -0500

    Removed firstCase from Results switch statement, making it DRYER. Added .vscode file to gitignore

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
-function firstCase(weap) {
-  return weap.charAt(0).toUpperCase() + weap.slice(1)
+function firstCase(statement) {
+  return <h4>{statement.charAt(0).toUpperCase() + statement.slice(1)}</h4>
 }
 
 function winCheck(human, computer, statement) {
-  if (human.wins === 5) {
-    return <h4>{`${statement.slice(0, -1)} and you win!`}</h4>
-  } else if (computer.wins === 5) {
-    return <h4>{`${statement.slice(0, -1)} and ${computer.name} wins...`}</h4>
-  } else {
-    return <h4>{statement}</h4>
+  function winnerText() {
+    if (human.wins === 5) {
+      return `${statement.slice(0, -1)} and you win!`
+    } else if (computer.wins === 5) {
+      return `${statement.slice(0, -1)} and ${computer.name} wins...`
+    } else {
+      return statement
+    }
   }
+  const winnerResult = winnerText()
+  return firstCase(winnerResult)
 }
 
 export default function Results({ results, human, computer, arsenal }) {
@@ -20,25 +24,25 @@ export default function Results({ results, human, computer, arsenal }) {
       case '0,0':
         return 'Tie!!!  Back to the pit!'
       case '0,3':
-        return `${firstCase(arsenal[0])} crushes ${arsenal[3]}!`
+        return `${arsenal[0]} crushes ${arsenal[3]}!`
       case '0,2':
-        return `${firstCase(arsenal[0])} crushes ${arsenal[2]}!`
+        return `${arsenal[0]} crushes ${arsenal[2]}!`
       case '1,0':
-        return `${firstCase(arsenal[1])} covers ${arsenal[0]}!`
+        return `${arsenal[1]} covers ${arsenal[0]}!`
       case '1,4':
-        return `${firstCase(arsenal[1])} disproves ${arsenal[4]}!`
+        return `${arsenal[1]} disproves ${arsenal[4]}!`
       case '2,1':
-        return `${firstCase(arsenal[2])} cuts ${arsenal[1]}!`
+        return `${arsenal[2]} cuts ${arsenal[1]}!`
       case '2,3':
-        return `${firstCase(arsenal[2])} decapitates ${arsenal[3]}!`
+        return `${arsenal[2]} decapitates ${arsenal[3]}!`
       case '3,4':
-        return `${firstCase(arsenal[3])} poisons ${arsenal[4]}!`
+        return `${arsenal[3]} poisons ${arsenal[4]}!`
       case '3,1':
-        return `${firstCase(arsenal[3])} eats ${arsenal[1]}!`
+        return `${arsenal[3]} eats ${arsenal[1]}!`
       case '4,2':
-        return `${firstCase(arsenal[4])} smashes ${arsenal[2]}!`
+        return `${arsenal[4]} smashes ${arsenal[2]}!`
       case '4,0':
-        return `${firstCase(arsenal[4])} vaporizes ${arsenal[0]}!`
+        return `${arsenal[4]} vaporizes ${arsenal[0]}!`
       default:
         return 'Something has gone terribly wrong...'
     }
